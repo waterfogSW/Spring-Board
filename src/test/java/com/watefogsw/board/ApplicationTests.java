0021c56d0c825d9840bd15cc70b8e5480248ab5c
waterfog9580 <waterfog9580@gmail.com>
2022-11-19 08:18:49 +0900

    :tada: project setup

---
+++ src/test/java/com/watefogsw/board/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.watefogsw.board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}

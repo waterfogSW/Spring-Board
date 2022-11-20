0076fd14321997ba4e150a4c0a341c4a61b01df5
waterfog9580 <waterfog9580@gmail.com>
2022-11-20 13:26:34 +0900

    :sparkles: webclient 응답 객체 수정

--- src/main/java/com/watefogsw/board/oauth/service/dto/OAuthTokenResponse.java
+++ src/main/java/com/watefogsw/board/oauth/service/dto/OAuthTokenResponse.java
@@ -1,8 +1,14 @@
 package com.watefogsw.board.oauth.service.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public record OAuthTokenResponse(
+    @JsonProperty("access_token")
     String accessToken,
+
     String scope,
+
+    @JsonProperty("token_type")
     String tokenType
 ) {
 

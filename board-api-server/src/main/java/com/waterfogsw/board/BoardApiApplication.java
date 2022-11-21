c563ba596067852e7883fef85022cd621610183a
waterfog9580 <waterfog9580@gmail.com>
2022-11-21 23:50:01 +0900

    :sparkles: add board-api-server module

---
+++ board-api-server/src/main/java/com/waterfogsw/board/BoardApiApplication.java
@@ -0,0 +1,13 @@
+package com.waterfogsw.board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoardApiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(BoardApiApplication.class, args);
+  }
+
+}

db608af5a16130deeb5477e4d5913468c1b552dd
waterfog9580 <waterfog9580@gmail.com>
2022-11-28 19:29:41 +0900

    :sparkles: role 변경

--- board-core/src/main/java/com/waterfogsw/board/core/user/domain/Role.java
+++ board-core/src/main/java/com/waterfogsw/board/core/user/domain/Role.java
@@ -1,15 +1,7 @@
 package com.waterfogsw.board.core.user.domain;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-
-@Getter
-@RequiredArgsConstructor
 public enum Role {
 
-  GUEST("ROLE_GUEST"),
-  USER("ROLE_USER");
-
-  private final String key;
+  ADMIN, USER
 
 }

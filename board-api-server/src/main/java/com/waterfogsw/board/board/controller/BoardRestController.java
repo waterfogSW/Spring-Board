8348a38f72dc29ba7044ee3951753fdbc2e1f5ba
waterfog9580 <waterfog9580@gmail.com>
2022-12-03 20:31:10 +0900

    :recycle: rename BoardService to BoardCommandService

--- board-api-server/src/main/java/com/waterfogsw/board/board/controller/BoardRestController.java
+++ board-api-server/src/main/java/com/waterfogsw/board/board/controller/BoardRestController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,7 +15,7 @@ import com.waterfogsw.board.board.dto.BoardCreateRequest;
 import com.waterfogsw.board.board.dto.BoardSearchRequest;
 import com.waterfogsw.board.board.dto.BoardSearchResponse;
 import com.waterfogsw.board.board.service.BoardQueryService;
-import com.waterfogsw.board.board.service.BoardService;
+import com.waterfogsw.board.board.service.BoardCommandService;
 import com.waterfogsw.board.common.auth.Auth;
 import com.waterfogsw.board.core.user.domain.Role;
 
@@ -27,14 +28,14 @@ import lombok.extern.slf4j.Slf4j;
 @RequestMapping("api/v1/boards")
 public class BoardRestController {
 
-  private final BoardService boardService;
+  private final BoardCommandService boardCommandService;
   private final BoardQueryService boardQueryService;
 
   @Auth(role = Role.USER)
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
   public void create(@RequestBody BoardCreateRequest request) {
-    boardService.create(request);
+    boardCommandService.create(request);
   }
 
   @GetMapping
@@ -42,5 +43,13 @@ public class BoardRestController {
   public List<BoardSearchResponse> getSliceOfBoard(BoardSearchRequest request) {
     return boardQueryService.getSliceOfBoard(request);
   }
+
+  @Auth(role = Role.USER)
+  @PutMapping
+  @ResponseStatus(HttpStatus.OK)
+  public void update() {
+
+  }
+
 
 }

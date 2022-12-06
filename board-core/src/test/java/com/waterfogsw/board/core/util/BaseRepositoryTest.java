220085152dfc3c271cc1fd78426b3169c64bfb08
waterfog9580 <waterfog9580@gmail.com>
2022-12-06 15:58:48 +0900

    :adhesive_bandage: fix TestContainer setup

--- board-core/src/test/java/com/waterfogsw/board/core/util/BaseRepositoryTest.java
+++ board-core/src/test/java/com/waterfogsw/board/core/util/BaseRepositoryTest.java
@@ -1,12 +1,17 @@
 package com.waterfogsw.board.core.util;
 
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 @ActiveProfiles("test")
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 public class BaseRepositoryTest {
+
+  @Container
+  private static final GenericContainer<?> MY_SQL_CONTAINER = new GenericContainer<>("mysql/mysql-server:8.0");
 
 }

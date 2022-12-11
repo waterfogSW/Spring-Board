77d50c30f007fd5c242fe41df09f205288b7556b
waterfog9580 <waterfog9580@gmail.com>
2022-12-11 13:51:56 +0900

    :white_check_mark: impl update test

--- board-api-server/src/test/java/com/waterfogsw/board/board/service/BoardCommandServiceTest.java
+++ board-api-server/src/test/java/com/waterfogsw/board/board/service/BoardCommandServiceTest.java
@@ -1,7 +1,11 @@
 package com.waterfogsw.board.board.service;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.mockito.BDDMockito.*;
 
+import java.util.Optional;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
@@ -9,8 +13,11 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.test.util.ReflectionTestUtils;
 
 import com.waterfogsw.board.board.dto.BoardCreateRequest;
+import com.waterfogsw.board.board.dto.BoardUpdateRequest;
+import com.waterfogsw.board.common.exception.AuthenticationException;
 import com.waterfogsw.board.core.board.domain.Board;
 import com.waterfogsw.board.core.board.repository.BoardRepository;
 import com.waterfogsw.board.core.user.domain.User;
@@ -50,6 +57,80 @@ class BoardCommandServiceTest {
 
         //then
         verify(boardRepository).save(any(Board.class));
+      }
+
+    }
+
+  }
+
+  @Nested
+  @DisplayName("update 메서드는")
+  class DescribeUpdate {
+
+    @Nested
+    @DisplayName("게시판 소유자가 현재 유저가 아니면")
+    class ContextWithCurrentUserIsNotBoardOwner {
+
+      @Test
+      @DisplayName("AuthenticationException 을 발생시킨다")
+      void ItThrowsException() {
+        //given
+        long currentUserId = 1L;
+        long boardOwnerId = 2L;
+
+        User currentUser = TestDataGenerator.getUser("panic", "sharea_nicholson2oeu@scale.nbe");
+        User boardOwner = TestDataGenerator.getUser("test", "gamalier_shelton5lq@pushing.bqj");
+        ReflectionTestUtils.setField(currentUser, "id", currentUserId);
+        ReflectionTestUtils.setField(boardOwner, "id", boardOwnerId);
+
+        Board board = TestDataGenerator.getBoard("below", "wordpress", boardOwner);
+
+        given(userService.getCurrentUser()).willReturn(currentUser);
+        given(boardRepository.findById(anyLong())).willReturn(Optional.of(board));
+
+        BoardUpdateRequest request = new BoardUpdateRequest("test", "test");
+
+        //when, then
+        assertThatThrownBy(() -> boardCommandService.update(1L, request)).isInstanceOf(AuthenticationException.class);
+      }
+
+    }
+
+    @Nested
+    @DisplayName("현재 유저가 게시판 소유자이면")
+    class ContextWithCurrentUserIsBoardOwner {
+
+      @Test
+      @DisplayName("board의 내용을 수정한다")
+      void ItUpdateBoard() {
+        //given
+        String updateTitle = "test";
+        String updateDescription = "test";
+        long currentUserId = 1L;
+        long boardOwnerId = 1L;
+        long boardId = 1L;
+        BoardUpdateRequest request = new BoardUpdateRequest(updateTitle, updateDescription);
+
+        User currentUser = TestDataGenerator.getUser("panic", "sharea_nicholson2oeu@scale.nbe");
+        User boardOwner = TestDataGenerator.getUser("test", "gamalier_shelton5lq@pushing.bqj");
+        Board board = TestDataGenerator.getBoard("below", "wordpress", boardOwner);
+
+        ReflectionTestUtils.setField(currentUser, "id", currentUserId);
+        ReflectionTestUtils.setField(boardOwner, "id", boardOwnerId);
+        ReflectionTestUtils.setField(board, "id", boardId);
+
+        given(userService.getCurrentUser()).willReturn(currentUser);
+        given(boardRepository.findById(anyLong())).willReturn(Optional.of(board));
+
+        //when
+        boardCommandService.update(boardId, request);
+
+        //then
+        Assertions.assertThat(board.getTitle())
+                  .isEqualTo(updateTitle);
+        Assertions.assertThat(board.getDescription())
+                  .isEqualTo(updateDescription);
+
       }
 
     }

284f6b8c2fcc269193f3124dfced3df6095b2b7d
waterfog9580 <waterfog9580@gmail.com>
2022-12-18 20:56:26 +0900

    :sparkles: setup chat module

---
+++ board-chat-server/build.gradle.kts
@@ -0,0 +1,28 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("jvm") version "1.6.21"
+    kotlin("plugin.spring") version "1.6.21"
+}
+
+dependencies {
+    implementation(project(":board-core"))
+
+    //web
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    //mysql
+    runtimeOnly("mysql:mysql-connector-java")
+
+    //kotlin
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        freeCompilerArgs = listOf("-Xjsr305=strict")
+        jvmTarget = "17"
+    }
+}
---
+++ board-chat-server/src/main/java/com/waterfogsw/board/BoardChatApplication.kt
@@ -0,0 +1,11 @@
+package com.waterfogsw.board
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BoardChatApplication
+
+fun main(args: Array<String>) {
+    runApplication<BoardChatApplication>(*args)
+}
